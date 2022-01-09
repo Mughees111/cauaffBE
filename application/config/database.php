efb9845a39bf0e60501917e840a580f32ac8a86f
Mughees111 <mughees.abbas@gmail.com>
2022-01-09 17:59:02 +0500

    'db_debug' => FALSE,

--- application/config/database.php
+++ application/config/database.php
@@ -107,7 +107,7 @@ else {
 		'dbdriver' => 'mysqli',
 		'dbprefix' => '',
 		'pconnect' => FALSE,
-		'db_debug' => (ENVIRONMENT !== 'production'),
+		'db_debug' => FALSE,
 		'cache_on' => FALSE,
 		'cachedir' => '',
 		'char_set' => 'utf8',
