04ddd628435cae113f02672c886f743d82901678
Mughees111 <mughees.abbas@gmail.com>
2022-01-10 18:15:47 +0500

    urls fixes

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-$x = true;
+$x = false;
 if($x){
 	$db['default'] = array(
 		'dsn'	=> '',
